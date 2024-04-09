98542bc68acc9028dbf20764b0311a7967439c80
ryanjewik <89747716+ryanjewik@users.noreply.github.com>
2024-04-09 17:43:08 +0900

    trying to add images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.database)
     implementation(libs.firebase.common.ktx)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/tujapp/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/tujapp/ui/ProfileScreen.kt
@@ -1,43 +1,57 @@
 package com.example.tujapp.ui
 
+import android.content.Context
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.tujapp.data.DataSource
 import com.example.tujapp.data.User
 import com.example.tujapp.model.profilePic
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+
 
 @Composable
 fun ProfileScreen(
     currentUser: User?
 ) {
+    val context = LocalContext.current
+    var selectedImageUri: MutableState<Uri?> = remember{ mutableStateOf(null)}
+    val getContent = rememberLauncherForActivityResult(ActivityResultContracts.GetContent())
+    {
+        uri:Uri?->
+        selectedImageUri.value = uri
+    }
     //Text(text = "This is a profile screen")
     Column(
         modifier = Modifier.fillMaxSize(),
-        //verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         var profilePicsList:List<profilePic> = DataSource().loadProfilePic()
@@ -56,41 +70,95 @@ fun ProfileScreen(
                 .border(2.dp, Color.Red, CircleShape),
             contentScale = ContentScale.Crop
         )
-/*        if (currentUser != null) {
-            OutlinedCard {
-                Text(
-                    text = currentUser.name!!,
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Text(
-                    text = currentUser.bio!!,
-                    style = MaterialTheme.typography.bodyLarge
-                )
-                Text(
-                    text = currentUser.contact!!,
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Text(
-                    text = currentUser.graduationYear!!.toString(),
-                    style = MaterialTheme.typography.titleLarge
-                )
-
-            }
+        Button(
+            onClick = {
+                // Open gallery to select an image
+                getContent.launch("image/*")
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "Select Image")
         }
-        Card {
-            Row {
-                OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
-                    label = { Text(text = "Search Other Users") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Go")
+        Button(
+            onClick = {
+                // Upload image to Firebase or any other storage service
+                selectedImageUri.value?.let { uri ->
+                    uploadImageToFirebase(context, uri)
+                        //Toast.makeText(context, uri.toString(), Toast.LENGTH_SHORT).show()
                 }
-            }
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "Upload Image")
         }
-*/
+        /*        if (currentUser != null) {
+                    OutlinedCard {
+                        Text(
+                            text = currentUser.name!!,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        Text(
+                            text = currentUser.bio!!,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                        Text(
+                            text = currentUser.contact!!,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        Text(
+                            text = currentUser.graduationYear!!.toString(),
+                            style = MaterialTheme.typography.titleLarge
+                        )
+
+                    }
+                }
+                Card {
+                    Row {
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            label = { Text(text = "Search Other Users") },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Go")
+                        }
+                    }
+                }
+        */
     }
+}
+private fun uploadImageToFirebase(thisContext: Context, imageUri: Uri) {
+    // Implement your logic to upload the image to Firebase or any other storage service here
+    // You can use Firebase Storage to upload the image
+
+    var storageRef = FirebaseStorage.getInstance()
+    storageRef.getReference("images").child(System.currentTimeMillis().toString())
+        .putFile(imageUri).
+    addOnSuccessListener { task ->
+        task.metadata!!.reference!!.downloadUrl
+            .addOnSuccessListener {
+                Toast.makeText(thisContext, "Successful 1", Toast.LENGTH_SHORT).show()
+                val userId = FirebaseAuth.getInstance().currentUser!!.uid
+                val mapImage = mapOf(
+                    "url" to it.toString()
+                )
+                val databaseReference =
+                    FirebaseDatabase.getInstance().getReference("userImages")
+                databaseReference.child(userId).setValue(mapImage)
+                    .addOnSuccessListener {
+                        Toast.makeText(thisContext, "Successful 2", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener { error ->
+                        Toast.makeText(thisContext, it.toString(), Toast.LENGTH_SHORT).show()
+                    }
+
+
+            }.addOnFailureListener{
+                Toast.makeText(thisContext, "failure", Toast.LENGTH_SHORT).show()
+            }
+    }.addOnFailureListener{
+            Toast.makeText(thisContext, "failure 2", Toast.LENGTH_SHORT).show()
+        }
 }
