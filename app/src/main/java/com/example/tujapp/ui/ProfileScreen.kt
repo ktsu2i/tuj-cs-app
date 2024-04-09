9df988fc96ad3a3a59ccddbf6b9b716e5c11be23
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-04-10 06:47:00 +0900

    merged

--- app/src/main/java/com/example/tujapp/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/tujapp/ui/ProfileScreen.kt
@@ -12,20 +12,34 @@ import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,112 +49,229 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat.startActivityForResult
-import com.example.tujapp.R
+
+import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.tujapp.data.DataSource
 import com.example.tujapp.data.User
 import com.example.tujapp.model.profilePic
-import com.google.firebase.Firebase
+import com.example.tujapp.ui.auth.SignInActivity
+
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.storage
+
+import com.google.firebase.storage.ktx.storage
 
 
 @Composable
 fun ProfileScreen(
-    currentUser: User?
+    currentUser: User?,
+    navController: NavController
 ) {
     val context = LocalContext.current
-    //var selectedImageUri: MutableState<Uri?> = remember{ mutableStateOf(null)}
-    fun uploadImageToFirebaseStorage(imageUri: Uri){
-        val storage= Firebase.storage
-        val storageReference = storage.reference
-        val imageName = "${System.currentTimeMillis()}.jpg"
-        val imageRef = storageReference.child("images/$imageName")
-        imageRef.putFile(imageUri).addOnSuccessListener {
-                _ -> imageRef.downloadUrl.addOnSuccessListener { Toast.makeText(context, "image upload successful",
-            Toast.LENGTH_SHORT).show()
-        }}
-            .addOnFailureListener{ Toast.makeText(context, "image upload failed",
-                Toast.LENGTH_SHORT).show()}
 
-    }
-    val getContent = rememberLauncherForActivityResult(ActivityResultContracts.GetContent())
-    {
-            uri: Uri? ->
-                uri?.let { uploadImageToFirebaseStorage(it) }
-                Toast.makeText(context, "image selected", Toast.LENGTH_SHORT).show()
-        //selectedImageUri.value = uri
-    }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        var profilePicsList:List<profilePic> = DataSource().loadProfilePic()
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "U s e r   P r o f i l e", style = MaterialTheme.typography.headlineMedium)
-        Spacer(modifier = Modifier.height(12.dp))
-        var profile = profilePicsList[0]
-        Image(
-            painter = painterResource(id = profile.imageResourceId),
-            contentDescription = stringResource(id = profile.stringResourceId),
-            modifier = Modifier
-                //.fillMaxWidth()
-                .height(150.dp)
-                .width(150.dp)
-                .clip(CircleShape)
-                .border(2.dp, Color.Red, CircleShape),
-            contentScale = ContentScale.Crop
-        )
+    var currentUserData by remember { mutableStateOf<User?>(currentUser) }
+    var showDialog by remember { mutableStateOf(false) }
+    var newImageUri by remember { mutableStateOf<Uri?>(null) }
 
-        Button(
-            onClick = {
-                // Open gallery to select an image
-                getContent.launch("image/*")
-                      //openImagePicker()
-            },
-            modifier = Modifier.padding(top = 16.dp)
-        ) {
-            Text(text = "Select & Upload Image")
+    LaunchedEffect(currentUserData?.name) {
+        val currentUserRef = Firebase.database.reference.child("users").child(currentUserData?.uid.toString())
+        val storageRef = Firebase.storage.reference.child("users/${currentUserData?.uid.toString()}/profile.jpg")
+
+        currentUserRef.get().addOnSuccessListener { dataSnapshot ->
+            val currentUser = dataSnapshot.getValue(User::class.java)
+            currentUserData = currentUser
+        }.addOnFailureListener {
+            // error handling
         }
 
-        /*        if (currentUser != null) {
-                    OutlinedCard {
-                        Text(
-                            text = currentUser.name!!,
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                        Text(
-                            text = currentUser.bio!!,
-                            style = MaterialTheme.typography.bodyLarge
-                        )
-                        Text(
-                            text = currentUser.contact!!,
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                        Text(
-                            text = currentUser.graduationYear!!.toString(),
-                            style = MaterialTheme.typography.titleLarge
-                        )
+        storageRef.downloadUrl.addOnSuccessListener { uri ->
+            newImageUri = uri
+        }
+    }
 
-                    }
+    val pickImageLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        newImageUri = uri
+    }
+
+    Scaffold { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ){
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Image(
+                painter = rememberImagePainter(newImageUri.toString()),
+                contentDescription = "profile image",
+                modifier = Modifier
+                    .size(128.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(text = currentUserData?.name ?: "Name", style = MaterialTheme.typography.headlineSmall)
+            Text(text = currentUserData?.email ?: "Email", style = MaterialTheme.typography.bodySmall)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row {
+                Button(
+                    onClick = { showDialog = true },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(164, 30, 53)
+                    )
+                ) {
+                    Text(text = "Edit Profile")
                 }
-                Card {
-                    Row {
-                        OutlinedTextField(
-                            value = "",
-                            onValueChange = {},
-                            label = { Text(text = "Search Other Users") },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                        )
-                        Button(onClick = { /*TODO*/ }) {
-                            Text(text = "Go")
-                        }
-                    }
+
+                Spacer(modifier = Modifier.width(4.dp))
+
+                Button(
+                    onClick = { pickImageLauncher.launch("image/*") },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(164, 30, 53)
+                    )
+                ) {
+                    Text(text = "Update Icon")
                 }
-        */
+            }
+
+            if (showDialog) {
+                EditProfileDialog(
+                    currentUser = currentUserData,
+                    onDismiss = { showDialog = false },
+                    onUpdateSuccess = { updatedUser ->
+                        currentUserData = updatedUser
+                    }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(
+                onClick = {
+                Firebase.auth.signOut()
+
+                val intent = Intent(context, SignInActivity::class.java)
+                context.startActivity(intent)
+
+                (context as? Activity)?.finish()
+            },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Gray
+                )
+            ) {
+                Text(text = "Log out")
+            }
+        }
     }
 }
+
+
+@Composable
+fun EditProfileDialog(
+    currentUser: User?,
+    onDismiss: () -> Unit,
+    onUpdateSuccess: (User) -> Unit
+) {
+    var newUserName by remember { mutableStateOf(currentUser?.name ?: "") }
+//    var newImageUri by remember { mutableStateOf<Uri?>(null) }
+
+    Scaffold { innerPadding ->
+        AlertDialog (
+            containerColor = Color.White,
+            onDismissRequest = onDismiss,
+            title = { Text(text = "Edit Profile") },
+            text = {
+                Column {
+                    OutlinedTextField (
+                        value = newUserName,
+                        onValueChange = { newUserName = it },
+                        label = { Text(text = "Name") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            },
+            confirmButton = {
+                TextButton (
+                    onClick = {
+                        // update username
+                        if (newUserName.isNotEmpty() && currentUser != null) {
+                            updateProfileWithImage(currentUser.uid.toString(), newUserName, "") {
+                                onUpdateSuccess(User(currentUser.uid, currentUser.email, newUserName))
+                            }
+                            onDismiss()
+
+                            newUserName = ""
+                        }
+                    }
+                ) {
+                    Text(
+                        text = "Edit",
+                        color = Color(164, 30, 53)
+                    )
+                }
+            },
+            dismissButton = {
+                TextButton (onClick = onDismiss) {
+                    Text(
+                        text = "Cancel",
+                        color = Color.Black
+                    )
+                }
+            }
+        )
+    }
+}
+
+
+fun updateProfileWithImage(
+    userId: String,
+    userName: String,
+    imageUrl: String,
+    onSuccess: (User) -> Unit,
+) {
+    val userRef = Firebase.database.reference.child("users").child(userId)
+    val userUpdates = mapOf(
+        "name" to userName,
+        "imageUrl" to imageUrl,
+    )
+
+    userRef.updateChildren(userUpdates).addOnSuccessListener {
+        val updatedUser = User(userId, userName, imageUrl)
+        onSuccess(updatedUser)
+    }
+}
+
+
+fun uploadImageToFirebase(
+    userId: String,
+    imageUri: Uri?,
+    onSuccess: (String) -> Unit
+) {
+    val storageRef = Firebase.storage.reference.child("users/${userId}/profile.jpg")
+    val imageRef = storageRef.child("images")
+    val userRef = Firebase.database.reference.child("users").child(userId)
+
+    storageRef.putFile(imageUri!!).addOnSuccessListener { taskSnapshot ->
+        taskSnapshot.metadata?.reference?.downloadUrl?.addOnSuccessListener { uri ->
+            onSuccess(uri.toString())
+        }
+    }
+
+//    userRef.updateChildren(mapOf("imageUrl" to imageUri.toString()))
+}
+
--- app/src/main/java/com/example/tujapp/ui/UserScreen.kt
+++ app/src/main/java/com/example/tujapp/ui/UserScreen.kt
@@ -1,11 +1,14 @@
 package com.example.tujapp.ui
 
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
@@ -40,6 +44,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
@@ -47,6 +52,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.tujapp.R
 import com.example.tujapp.data.Post
 import com.example.tujapp.data.Reply
@@ -57,6 +63,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -64,5 +71,52 @@ fun UserScreen (
     userId: String,
     navController: NavController
 ) {
-    Text(text = "$userId")
+    val userData = remember { mutableStateOf<User?>(null) }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    LaunchedEffect(Unit) {
+        val databaseRef = Firebase.database.reference
+
+        // fetch the author (user who posted it)
+        val userRef = databaseRef.child("users").child(userId)
+
+        userRef.get().addOnSuccessListener { dataSnapshot ->
+            val user = dataSnapshot.getValue(User::class.java)
+            userData.value = user
+
+            // fetch the user profile image
+            val storageRef = Firebase.storage.reference.child("users/${userId}/profile.jpg")
+
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                imageUri = uri
+            }
+        }.addOnFailureListener {
+            // if failed to fetch the current user
+        }
+    }
+
+    Scaffold { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ){
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Image(
+                painter = if (imageUri == null) painterResource(id = R.drawable.user_profile_icon) else rememberImagePainter(imageUri.toString()),
+                contentDescription = "profile image",
+                modifier = Modifier
+                    .size(128.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(text = userData.value?.name ?: "Name", style = MaterialTheme.typography.headlineSmall)
+            Text(text = userData.value?.email ?: "Email", style = MaterialTheme.typography.bodySmall)
+        }
+    }
 }
